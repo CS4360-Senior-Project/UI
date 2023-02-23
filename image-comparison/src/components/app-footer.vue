8cc12f77f721c81295da0d7d488831118e9645a6
millerac38 <73674898+millerac38@users.noreply.github.com>
2023-02-22 21:31:37 -0700

    create app footer

---
+++ image-comparison/src/components/app-footer.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-footer class="text-caption text-uppercase flex-grow-0" color="#3C5556">
+        <div style="color: white">
+            &copy; {{ getDate }} Team Data - UI v{{ appVersion }}
+        </div>
+    </v-footer>
+</template>
+
+<script>
+    export default {
+        name: 'AppFooter'
+    }
+</script>
+
+<script setup>
+import {version} from "../../package.json"
+
+const appVersion = version
+console.log("APP VERSION: ", appVersion)
+
+const getDate = new Date().getFullYear()
+</script>
