1e4725928ffb1e631b4641b61171701d11dd1e3b
millerac38 <73674898+millerac38@users.noreply.github.com>
2023-02-23 17:40:19 -0700

    changing colors

--- image-comparison/src/App.vue
+++ image-comparison/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app style="background-color: #E9F1EF; color: #3C5556">
+  <v-app style="background-color: #f4ede7" >
     <!--v-parallax src="https://i0.wp.com/www.knoxalliance.store/wp-content/uploads/2017/05/light-color-background-images-for-website-top-hd-images-for-free-light-color-background-images-for-website-1-1024x768.jpg?ssl=1"></v-parallax-->
     <AppHeader/>
     <v-main>
